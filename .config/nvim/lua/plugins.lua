c8a570cd710be7a9481bbba77a528154189e3ccd
Jamie <jamie@javahound.org>
2024-09-27 14:59:34 +0200

    feat: add nvim config & plugins

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  "nvim-java/nvim-java",
+  "pmizio/typescript-tools.nvim",
+  "Zeioth/compiler.nvim",
+  "folke/neodev.nvim",
+  "folke/which-key.nvim",
+  { "AstroNvim/AstroNvim", version = "^4", import = "astronvim.plugins" },
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+}
